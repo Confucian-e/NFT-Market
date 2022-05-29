5329de89af9f5b6d2ee35d36bc549734ce7a2d59
Confucian_e <534076476@qq.com>
2022-05-29 20:12:22 +0800

    update Market.sol

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
-contract Market is ERC721 {
+contract Market is ERC721, ERC20 {
     // 替换地址
     address constant TOKEN_BUY = 0xCFf94b4606c1e3D73510A80d9868D9B07825D692;
     address constant TOKEN_POINT = 0xCFf94b4606c1e3D73510A80d9868D9B07825D692;
@@ -19,18 +20,15 @@ contract Market is ERC721 {
         creator = msg.sender;
     }
 
-    uint balanceOfBuy = IERC20(TOKEN_BUY).balanceOf(address(this));
-    uint balanceOfPoint = IERC20(TOKEN_POINT).balanceOf(address(this));
-
     event GetTokenBuy(address indexed user, uint amount);
     event BuyNFT(address indexed buyer, uint price, uint indexed id);
     event Refund(uint time);
     event Swap(address indexed user, address tokenIn, address tokenOut, uint amountIn, uint amountOut);
 
-    // 用户兑换 TokenBuy
+    // 用户兑换 TokenBuy，兑换比：1:100
     function getTokenBuy() public payable {
         uint amount = msg.value / 1 ether;
-        IERC20(TOKEN_BUY).transfer(msg.sender, amount * 100);
+        IERC20(TOKEN_BUY).transfer(msg.sender, amount * 10000);
 
         emit GetTokenBuy(msg.sender, amount);
     }
@@ -59,6 +57,8 @@ contract Market is ERC721 {
     // 创建者提取合约内 ERC20 代币
     function refund() external {
         require(msg.sender == creator, "Only creator can refund!");
+        uint balanceOfBuy = IERC20(TOKEN_BUY).balanceOf(address(this));
+        uint balanceOfPoint = IERC20(TOKEN_POINT).balanceOf(address(this));
         IERC20(TOKEN_BUY).transfer(creator, balanceOfBuy);
         IERC20(TOKEN_POINT).transfer(creator, balanceOfPoint);
 
@@ -67,19 +67,23 @@ contract Market is ERC721 {
 
     // TokenBuy 和 TokenPoint 建个池子
     function swap(address tokenIn, uint amountIn) public {
-        uint total = balanceOfBuy * balanceOfPoint;
+        uint BuyBeforeSwap = IERC20(TOKEN_BUY).balanceOf(address(this));
+        uint PointBeforeSwap = IERC20(TOKEN_POINT).balanceOf(address(this));
+        uint total = BuyBeforeSwap * PointBeforeSwap;
         
         if(tokenIn == TOKEN_BUY) {
             // Buy 换 Point
             IERC20(TOKEN_BUY).transferFrom(msg.sender, address(this), amountIn);
-            uint amountOutPoint = total / balanceOfBuy;
+            uint BuyAfterSwap = IERC20(TOKEN_BUY).balanceOf(address(this));
+            uint amountOutPoint = total / BuyAfterSwap;
             IERC20(TOKEN_POINT).transfer(msg.sender, amountOutPoint);
 
             emit Swap(msg.sender, tokenIn, TOKEN_POINT, amountIn, amountOutPoint);
         } else {
             // Point 换 Buy
             IERC20(TOKEN_POINT).transferFrom(msg.sender, address(this), amountIn);
-            uint amountOutBuy = total / balanceOfPoint;
+            uint PointAfterSwap = IERC20(TOKEN_BUY).balanceOf(address(this));
+            uint amountOutBuy = total / PointAfterSwap;
             IERC20(TOKEN_BUY).transfer(msg.sender, amountOutBuy);
 
             emit Swap(msg.sender, tokenIn, TOKEN_BUY, amountIn, amountOutBuy);
